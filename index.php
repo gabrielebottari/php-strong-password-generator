5b92d8f9e3856ac828f2d0fb3ef5341e2a0da744
gabrielebottari <gabriele.bottari1@gmail.com>
2024-02-07 13:40:05 +0100

    ADD base structure

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-    echo 'Template PHP';
+
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -7,12 +9,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
+        <title>Strong Password Generator</title>
     </head>
     <body>
 
         <header>
-            HEADER
+            Strong Password Generator
         </header>
 
         <main>
