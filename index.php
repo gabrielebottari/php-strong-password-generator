c3c3ed43e988b87283d339158cb4c510e1102736
gabrielebottari <gabriele.bottari1@gmail.com>
2024-02-07 14:24:46 +0100

    ADD milestone 2

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
 
-    //Funzione per generare la password casuale
-    function passwordGenerator($lunghezza = 8) {
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
-        $casualPassword = '';
-        for ($i = 0; $i < $lunghezza; $i++) {
-            $casualPassword .= $characters[rand(0, strlen($characters) - 1)];
-        }
-        return $casualPassword;
-    }
-    
-    // Cattura la lunghezza dalla richiesta GET e genera la password
-    $passwordLength = '';
-
-    if (isset($_GET['length'])) {
-        $passwordLength = (int)$_GET['length'];
-    }
-
-    $generatedPassword = passwordGenerator($passwordLength);
-    $viewPassword = isset($_GET['length']);
+    include __DIR__."/partials/functions.php";
 
 ?>
 
