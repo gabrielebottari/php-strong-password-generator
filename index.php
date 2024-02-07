8561ea1a106e2a035bfb31fef2404964e7596f1e
gabrielebottari <gabriele.bottari1@gmail.com>
2024-02-07 16:15:42 +0100

    ADD milestone 4 END exercise

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     </head>
     <body class="bg-primary-subtle">
 
-        <div class="container">
+        <div class="container py-5">
 
             <header class="p-5">
                 <h1 class="text-center text-uppercase text-primary">
@@ -25,13 +25,32 @@
 
             <main>
                 <form class="row justify-content-center" action="index.php" method="get">
-                    <div class="col-5">
+
+                    <div class="col-2 text-center">
                         <label class="form-label text-primary" for="length">Lunghezza Password:</label>
                         <input class="form-control" type="number" id="length" name="length" min="8" max="32" required>
                     </div>
-                    <div class="col-3 align-self-end text-center">
+
+                    <div class="col-8 align-self-end text-center btn-group" role="group" required>
+
+                        <input class="btn-check"  type="checkbox" id="numbers" name="numbers" checked autocomplete="off"> 
+                        <label class="btn btn-outline-primary" for="numbers">Includi Numeri</label>
+                        
+                        <input class="btn-check" type="checkbox" id="letters" name="letters" checked autocomplete="on">
+                        <label class="btn btn-outline-primary" for="letters">Includi Lettere</label>
+                        
+                        <input class="btn-check" type="checkbox" id="symbols" name="symbols" checked autocomplete="off">
+                        <label class="btn btn-outline-primary" for="symbols">Includi Simboli</label>
+                        
+                        <input class="btn-check" type="checkbox" id="repeats" name="repeats" checked autocomplete="off">
+                        <label class="btn btn-outline-primary" for="repeats">Permetti Ripetizioni</label>
+
+                    </div>
+
+                    <div class="col-2 align-self-end text-center d-flex flex-column">
                         <button class="btn btn-primary" type="submit">Genera Password</button>
                     </div>
+
                 </form>
             </main>
 
--- partials/functions.php
+++ partials/functions.php
@@ -1,6 +1,7 @@
 <?php
 
     //Funzione per generare la password casuale
+    /*
     function passwordGenerator($passwordLength) {
         $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
         $casualPassword = '';
@@ -9,6 +10,38 @@
         }
         return $casualPassword;
     }
+    */
+
+    function passwordGenerator($passwordLength, $includeNumbers, $includeLetters, $includeSymbols, $allowRepeats) {
+        $characters = '';
+        if ($includeLetters) {
+            $characters .= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        if ($includeNumbers) {
+            $characters .= '0123456789';
+        }
+        if ($includeSymbols) {
+            $characters .= '!@#$%^&*()_+-=[]{}|;:,.<>?';
+        }
+    
+        $casualPassword = '';
+        $notDoubleCharacters = '';
+    
+        for ($i = 0; $i < $passwordLength; $i++) {
+            $newCharacters = $characters[rand(0, strlen($characters) - 1)];
+    
+            if (!$allowRepeats) {
+                while ($newCharacters == $notDoubleCharacters) {
+                    $newCharacters = $characters[rand(0, strlen($characters) - 1)];
+                }
+            }
+    
+            $casualPassword .= $newCharacters;
+            $notDoubleCharacters = $newCharacters;
+        }
+        return $casualPassword;
+    }
+    
     
     /*
     $passwordLength = '';
@@ -23,13 +56,18 @@
 
     if (isset($_GET['length'])) {
         $passwordLength = (int)$_GET['length'];
-        $generatedPassword = passwordGenerator($passwordLength);
+        $includeNumbers = isset($_GET['numbers']);
+        $includeLetters = isset($_GET['letters']);
+        $includeSymbols = isset($_GET['symbols']);
+        $allowRepeats = isset($_GET['repeats']);
+        $generatedPassword = passwordGenerator($passwordLength, $includeNumbers, $includeLetters, $includeSymbols, $allowRepeats);
         
         //Salva la password generata nella sessione
         $_SESSION['generatedPassword'] = $generatedPassword;
         
         //Redirect alla pagina di visualizzazione
         header('Location: partials/password.php');
+
     }
 
 ?>
